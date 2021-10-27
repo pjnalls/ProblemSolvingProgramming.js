a42769bc9c681dd85157f9f08a90535a977b18f9
pjnalls <pjnalls.js@gmail.com>
2021-10-27 09:05:40 -0500

    Create `example.js` file & add comments for steps.

---
+++ example.js
@@ -0,0 +1,13 @@
+/** Carefully read (and/or listen to) the problem statement while assuming that there is something you do not know. */
+
+/** Re-read (or ask the speaker to repeat) the problem statement as many times as you need to get the gist of what the problem statement requires. */
+
+/** Formulate and ask your questions rather than assuming you get it or understand completely. If you're confused, be comfortable with your confusion as this gives you the opportunity to ask multiple questions until you have the clarity you need before start programming in confidence. */
+
+/** Start brainstorming an initial, basic solution with short-handed (or long-handed, if you have time,) comments of each step of the algorithm before you code as you explain each step in the algorithm, why you chose each step, and what each step does. */
+
+/** Start testing or event-storming your brainstorming for your solution with "what-if" scenarios. You can use your data inputs to test-drive the design of your basic solution — accounting for the base cases (from initial brainstormed solution), and then for the best, average, worst, and edge cases. */
+
+/** Summarize your completed, brainstormed, and (now) event-stormed solution to yourself and audibly to others to finalize your solution design until you and everyone else has clarity and now the confidence needed your your solution. */
+
+/** Finally, you can start confidently programming your solution. Remember, the more low-tech your means for programming (e.g., pencil and paper, Notepad, etc.) the harder it is, yet the better programmer you'll become. */
