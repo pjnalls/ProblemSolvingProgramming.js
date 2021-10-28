cdf1ca430803e4882c6b0f047a20034c30ae794d
pjnalls <pjnalls.js@gmail.com>
2021-10-28 06:55:35 -0500

    Create `testCases` array for collection of inputs.

--- example.js
+++ example.js
@@ -1,8 +1,50 @@
 /** Carefully read (and/or listen to) the problem statement while assuming that there is something you do not know. */
 
+/**
+ *
+ * READ PROBLEM STATEMENT:
+ *
+ * Given an array of integers and a value, determine if there are any two integers in the array whose sum is equal to the given value.
+ *
+ * (...? What are my initial questions about this problem statement?)
+ */
+
 /** Re-read (or ask the speaker to repeat) the problem statement as many times as you need to get the gist of what the problem statement requires. */
 
+/**
+ *
+ * RE-READ TO "SEE INTO" PROBLEM STATEMENT:
+ *
+ * Given an array of integers (e.g., "[-1, 0, 3, 4, -3]"?) and a value (7?), determine if there are any two integers in the array whose sum (addition of only 2 integer values in the array?) is equal to the given value (7?).
+ *
+ * (Okay... so I've "got the gist" enough from re-reading this problem statement to start clearing the vagueness and ambiguity in the statement to derive unequivical meaning of what is required here.)
+ *
+ */
+
 /** Formulate and ask your questions rather than assuming you get it or understand completely. If you're confused, be comfortable with your confusion as this gives you the opportunity to ask multiple questions until you have the clarity you need before start programming in confidence. */
+
+/**
+ *
+ * FORMULATE AND ASK QUESTIONS FOR ASSURED CLARITY OF THE PROBLEM:
+ *
+ * Given an array of integers (e.g., "[-1, 0, 3, 4, -3]"?) and a value (e.g., 7?), determine if there are any two integers in the array whose sum (addition of only 2 integer values in the array?) is equal to the given value (7?).
+ */
+
+const testCases = [
+  /** Base Case? */
+  {
+    integers: [-1, 0, 3, 4, -3],
+    value: 7,
+  },
+];
+
+/**
+ * 1. "Given an array... and a value..." <= Does this mean we will always be guaranteed to receive the correct data types for our input data given for our algorithm?
+ */
+
+/**
+ * 2. "Given an array of integers..." <= What is the largest and smallest integer values? Are these values positive, negative, and/or include zero, or any possible integer which can be represented in JavaScript?
+ */
 
 /** Start brainstorming an initial, basic solution with short-handed (or long-handed, if you have time,) comments of each step of the algorithm before you code as you explain each step in the algorithm, why you chose each step, and what each step does. */
 
