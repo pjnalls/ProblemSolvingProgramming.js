7914112cbc7b38a6cd525c0bec6324a6a3acd510
pjnalls <pjnalls.js@gmail.com>
2021-10-29 05:28:29 -0500

    Add `Prettier` multi-line horizontal rule.

--- example.js
+++ example.js
@@ -1,5 +1,11 @@
-/** Carefully read (and/or listen to) the problem statement 
- * while assuming that there is something you do not know. */
+/** 
+ * 
+ * 
+ * Carefully read (and/or listen to) the problem statement 
+ * while assuming that there is something you do not know. 
+ * 
+ * 
+ * */
 
 /**
  *
@@ -12,9 +18,19 @@
  * (...? What are my initial questions about this problem statement?)
  */
 
-/** Re-read (or ask the speaker to repeat) the problem 
+/**
+ ******************************************************************
+ */
+
+/** 
+ * 
+ * 
+ * Re-read (or ask the speaker to repeat) the problem 
  * statement as many times as you need to get the gist 
- * of what the problem statement requires. */
+ * of what the problem statement requires. 
+ * 
+ * 
+ * */
 
 /**
  *
@@ -33,11 +49,19 @@
  *
  */
 
-/** Formulate and ask your questions rather than assuming you 
+/**
+ ******************************************************************
+ */
+
+/** 
+ * 
+ * Formulate and ask your questions rather than assuming you 
  * get it or understand completely. If you're confused, 
  * be comfortable with your confusion as this gives you 
  * the opportunity to ask multiple questions until you 
- * have the clarity you need before start programming in confidence. */
+ * have the clarity you need before start programming in confidence. 
+ * 
+ * */
 
 /**
  *
@@ -122,11 +146,21 @@ const testCases = [];
  * represented in JavaScript?
  */
 
-/** Start brainstorming an initial, basic solution with 
+/**
+ ******************************************************************
+ */
+
+/** 
+ * 
+ * 
+ * Start brainstorming an initial, basic solution with 
  * short-handed (or long-handed, if you have time,) 
  * comments of each step of the algorithm before you 
  * code as you explain each step in the algorithm, 
- * why you chose each step, and what each step does. */
+ * why you chose each step, and what each step does. 
+ * 
+ * 
+ * */
 
 /* 
  * Sum of Two Integers
@@ -147,11 +181,21 @@ const testCases = [];
  *
  */
 
-/** Start testing or event-storming your brainstorming for your solution 
+/**
+ ******************************************************************
+ */
+
+/** 
+ * 
+ * 
+ * Start testing or event-storming your brainstorming for your solution 
  * with "what-if" scenarios. You can use your data inputs to test-drive 
  * the design of your basic solution — accounting for the base cases 
  * (from initial brainstormed solution), and then for the best, average, 
- * worst, and edge cases. */
+ * worst, and edge cases. 
+ * 
+ * 
+ * */
 
 // Base (Average, `true`) Case:
 testCase = new TestCase([-1, 0, 3, 4, -3], 7).data;
@@ -225,10 +269,24 @@ testCase && testCases.push(testCase);
 
 testCases.forEach((tc) => console.log(`[[${tc.integers}], ${tc.value}]`));
 
-/** Summarize your completed, brainstormed, and (now) event-stormed 
+/**
+ ******************************************************************
+ */
+
+/** 
+ * 
+ * 
+ * Summarize your completed, brainstormed, and (now) event-stormed 
  * solution to yourself and audibly to others to finalize your solution 
  * design until you and everyone else has clarity and now the confidence 
- * needed your your solution. */
+ * needed your your solution. 
+ * 
+ * 
+ * */
+
+/**
+ ******************************************************************
+ */
 
 /** 7. Finally, you can start confidently developing your solution and 
  * debug with your `testCases`. Remember, the more low-tech your means 
@@ -260,3 +318,7 @@ const sumOfTwoIntegers = (integers, value) => {
 };
 
 testCases.forEach((tc) => console.log(sumOfTwoIntegers(tc.integers, tc.value)));
+
+/**
+ ******************************************************************
+ */
